1bc8b095840e12188e12344e7e5dd5932b71b49c
Aravind Metku <64901333+aravindmetku@users.noreply.github.com>
2025-03-07 15:11:22 +0530

    removed unwanted code

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,6 +1,4 @@
-import FetchDataSteps from "@/components/tutorial/fetch-data-steps";
 import { createClient } from "@/utils/supabase/server";
-import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
 
 export default async function ProtectedPage() {
@@ -20,9 +18,6 @@ export default async function ProtectedPage() {
         src="https://docs.google.com/forms/d/e/1FAIpQLScX-UhKmbKFdn0izH7e5y8WpKpehhm3Q_CUSuhLPHjFn-FNug/viewform?embedded=true"
         width="640"
         height="466"
-        frameBorder="0"
-        marginHeight="0"
-        marginWidth="0"
       >
         Loading…
       </iframe>
